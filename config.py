e220de3d947b78e674418b4189540ed77a461256
Ryan Tri <ry4ntr1@gmail.com>
2024-06-24 12:04:57 -0500

    Initialized: config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+MODELS_DIR = BASE_DIR / 'models'
+TRAINING_DIR = BASE_DIR / 'training' 
+
+# Data Directories
+DATA_DIR = BASE_DIR / 'data'
+KEYPOINTS_DIR = DATA_DIR / 'keypoints'
+TENNIS_BALL_DIR = DATA_DIR / 'tennis_balls'
+
+directories = [DATA_DIR, MODELS_DIR, TRAINING_DIR, TENNIS_BALL_DIR, KEYPOINTS_DIR]
+for directory in directories:
+    directory.mkdir(parents=True, exist_ok=True)
+    
