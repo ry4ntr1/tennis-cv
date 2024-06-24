8249ff3de218cb5826645c1195cac0269ccfe741
Ryan Tri <ry4ntr1@gmail.com>
2024-06-24 11:59:37 -0500

    Initialized: requirement.txt, setup.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tennis-cv
+
+1. Install Requirements
+2. Get RoboFlow API Key and Set .env File
+3. Run ball_detection.ipynb in ./training
+3. cd into tutorial and run test_yolo_inference.py 
+4. Upload this to cloud and run ball_detection.ipynb
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys
+from pathlib import Path
+
+
+parent_dir = Path('.').resolve().parent
+
+print(sys.path)
+
+if str(parent_dir) not in sys.path:
+    sys.path.append(str(parent_dir))
+    
+import config
+
+setup(
+    name='Tennis-Analysis-CV',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        line.strip() for line in open("requirements.txt", "r").readlines() if line.strip() and not line.strip().startswith('#')
+    ],
+    author='Ryan Tri',
+    author_email='ry4ntr1@gmail.com',
+    description='Lorem Ipsum',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ry4ntr1/tennis-cv', 
+    python_requires='>=3.11',  
+    include_package_data=True,
+    zip_safe=False
+)
