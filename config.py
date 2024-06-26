c975e58072ae038ed6dec47a2b49c4cd7b6cab92
Ryan Tri <ry4ntr1@gmail.com>
2024-06-25 22:22:50 -0500

    training/keypoints_detection.ipynb: Saves Keypoint Detection Model Weights, models/best.py: Object Detection Weights in Tennis Context, models/keypoints_model.pth: Keypoint Detection Weights

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ DATA_DIR = BASE_DIR / 'data'
 KEYPOINTS_DIR = DATA_DIR / 'keypoints'
 TENNIS_BALL_DIR = DATA_DIR / 'tennis_balls'
 
+UTILS_DIR = BASE_DIR / 'utils'
+
+
 directories = [DATA_DIR, MODELS_DIR, TRAINING_DIR, TENNIS_BALL_DIR, KEYPOINTS_DIR]
 for directory in directories:
     directory.mkdir(parents=True, exist_ok=True)
---
+++ main.py
@@ -0,0 +1,3 @@
+from utils import read_video, save_video
+
+
