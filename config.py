82757585eb65503738f674b808129451a09be9e9
Ryan Tri <ry4ntr1@gmail.com>
2024-06-25 23:37:44 -0500

    config.py: Update File To Save Tracker Stubs

--- config.py
+++ config.py
@@ -13,9 +13,10 @@ TENNIS_BALL_DIR = DATA_DIR / 'tennis_balls'
 # Utils Directory
 UTILS_DIR = BASE_DIR / 'utils'
 
-# Test Output Directory
+# Output Directories
 TEST_OUTPUT_DIR = BASE_DIR / 'test_output'
+TRACKER_STUB_DIR = BASE_DIR / 'tracker_stubs'
 
-directories = [ MODELS_DIR, TRAINING_DIR, SAMPLE_DATA_DIR,TEST_OUTPUT_DIR, UTILS_DIR, DATA_DIR, TENNIS_BALL_DIR, KEYPOINTS_DIR]
+directories = [ MODELS_DIR, TRAINING_DIR, SAMPLE_DATA_DIR,TEST_OUTPUT_DIR, TRACKER_STUB_DIR, UTILS_DIR, DATA_DIR, TENNIS_BALL_DIR, KEYPOINTS_DIR]
 for directory in directories:
     directory.mkdir(parents=True, exist_ok=True)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from utils import (load_video_frames, export_video)
-from config import (SAMPLE_DATA_DIR, TEST_OUTPUT_DIR, MODELS_DIR)
+from config import (SAMPLE_DATA_DIR, TEST_OUTPUT_DIR, MODELS_DIR, TRACKER_STUB_DIR)
 from trackers import PlayerTracker
 
 
@@ -13,7 +13,7 @@ def main():
     
     player_tracker = PlayerTracker(model_path=f"{MODELS_DIR}/best.pt")
     
-    player_detection = player_tracker.detect_frames(video_frames)  
+    player_detection = player_tracker.detect_frames(video_frames, read_from_stub=False, stub_path=f"{TRACKER_STUB_DIR}/player_detection.pkl")  
     
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detection)  
     
